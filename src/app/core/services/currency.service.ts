eaa7e9b8b77be98424fa8871c12622467f8573b9
Arthur Stofeles <arthurstofeles@MacBook-Air-de-Arthur.local>
2026-01-20 18:48:28 -0300

    feat: add HttpClient and fetch currencies from API

--- src/app/core/services/currency.service.ts
+++ src/app/core/services/currency.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Currency } from '../models/currency';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CurrencyService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private readonly API_URL =
+    'https://economia.awesomeapi.com.br/json/last/CAD-BRL,ARS-BRL,GBP-BRL';
+
+  getCurrencies(): Observable<Currency[]> {
+    return this.http
+      .get<any>(this.API_URL)
+      .pipe(map((response) => this.mapResponse(response)));
+  }
+  
+  private mapResponse(response: any): Currency[] {
+    return [
+      this.buildCurrency('CAD', response.CADBRL),
+      this.buildCurrency('ARS', response.ARSBRL),
+      this.buildCurrency('GBP', response.GBPBRL),
+    ];
+  }
+
+  private buildCurrency(code: string, data: any): Currency {
+    return {
+      code,
+      value: Number(data.bid),
+      variation: Number(data.pctChange),
+      updatedAt: new Date(Number(data.timestamp) * 1000),
+    };
+  }
 }
