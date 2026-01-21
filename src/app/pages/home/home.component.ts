9ae6bfe97f4d5ec59f49a87d81cb69bab1e22e27
Arthur Stofeles <arthurstofeles@MacBook-Air-de-Arthur.local>
2026-01-21 19:39:37 -0300

    refactor: use reactive currencies$ with auto refresh

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrencyService } from '../../core/services/currency.service';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { interval, Observable, startWith, Subscription, switchMap } from 'rxjs';
 import { Currency } from '../../core/models/currency';
 import { CurrencyCardComponent } from '../../shared/components/currency-card/currency-card.component';
 
@@ -10,19 +10,21 @@ import { CurrencyCardComponent } from '../../shared/components/currency-card/cur
   standalone: true,
   imports: [CommonModule, CurrencyCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-
   currencies$!: Observable<Currency[]>;
-  loading$! : Observable<boolean>
-  error$! : Observable<string | null>;
+  loading$!: Observable<boolean>;
+  error$!: Observable<string | null>;
 
-  constructor(private currencyService: CurrencyService) { }
+  constructor(private currencyService: CurrencyService) {}
 
   ngOnInit(): void {
     this.loading$ = this.currencyService.loading$;
     this.error$ = this.currencyService.error$;
-    this.currencies$ = this.currencyService.getCurrencies();
+    this.currencies$ = interval(3 * 60 * 1000).pipe(
+      startWith(0),
+      switchMap(() => this.currencyService.getCurrencies()),
+    );
   }
 }
